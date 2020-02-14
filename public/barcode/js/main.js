5969210d69d618054e8d47aee8e7ca8cd6c4594a
Luis <luis@vivomini.dhcp.nd.edu>
2020-02-14 16:24:16 -0500

    Commit inicial

---
+++ public/barcode/js/main.js
@@ -0,0 +1,70 @@
+window.addEventListener('load', function () {
+    let selectedDeviceId;
+    const codeReader = new ZXing.BrowserMultiFormatReader()
+    console.log('ZXing code reader initialized')
+    codeReader.getVideoInputDevices()
+        .then((videoInputDevices) => {
+        const sourceSelect = document.getElementById('sourceSelect')
+        selectedDeviceId = videoInputDevices[0].deviceId
+        
+        if (videoInputDevices.length > 1)
+        {
+            selectedDeviceId = videoInputDevices[1].deviceId
+            videoInputDevices.forEach((element) => {
+                console.log(`Label: ${element.label} DeviceID: ${element.deviceId}`);
+            });
+        }
+
+        document.getElementById('startButton').addEventListener('click', () => {
+            var startButton = document.getElementById('startButton');
+            if(startButton.getAttribute('data-status') == "start")
+            {
+                startButton.setAttribute('data-status','reset');
+
+                codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', async (result, err) => {
+                if (result) 
+                {
+                    console.log(result)
+                    document.getElementById('result').textContent = result.text
+
+                    let response_data = await post("/barcode").catch(message => console.log(message));
+                    let data = await response_data.json();
+                    console.log(data);
+                    document.getElementById('product').textContent = data.title;
+                    
+                }
+                if (err && !(err instanceof ZXing.NotFoundException)) {
+                    console.error(err)
+                    document.getElementById('result').textContent = err
+                }
+                })
+                console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
+            }
+            else
+            {
+                startButton.setAttribute('data-status','start');
+
+                codeReader.reset()
+                document.getElementById('result').textContent = '';
+                console.log('Reset.')
+            }
+        })
+
+        })
+        .catch((err) => {
+        console.error(err)
+        })
+    
+    function post(url,params={})
+    {
+      return fetch(url,{
+        method: 'POST',
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+          'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify(params)
+      });
+    }
+});
---
+++ src/barcode.cr
@@ -0,0 +1,45 @@
+# TODO: Write documentation for `Barcode`
+require "kemal"
+require "kemal-session"
+
+ROOT_PATH = "/Users/luis/Desktop/Code/Crystal/apps"
+# ROOT_PATH = "/var/www/domains/mischicanadas/subdomains/app"
+
+PUBLIC_PATH   = "#{ROOT_PATH}/warehouse/public"
+PORT = 3026
+
+public_folder PUBLIC_PATH
+
+module Barcode
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+  Kemal::Session.config.secret = "9e7abe8ae041296820a0b69ef0e4a397c87f5866f454d35d432840bc98cfd789439addc260bb6f9a058e88faa7e4a4e416e39d05273f459dd3373dc6387cf69c"
+
+  static_headers do |response, filepath, filestat|
+    response.headers.add("Cache-control", "public")
+    response.headers.add("Cache-control", "max-age=31557600")
+    response.headers.add("Cache-control", "s-max-age=31557600")
+    response.headers.add("Content-Size", filestat.size.to_s)
+  end
+
+  get "/barcode" do |env|
+    render "src/views/index.ecr"
+  end
+
+  post "/barcode" do |env|
+    env.response.content_type = "application/json"
+    upc = env.params.json["upc"].as(String)
+
+    uri = URI.parse("https://api.upcitemdb.com/prod/trial/lookup")
+    query = {"upc" => upc}.to_json
+    request = HTTP::Client.post(uri, 
+                                headers: HTTP::Headers{"content-type" => "application/json"}, 
+                                body: query
+                                )
+
+    response = request.body
+    {title: JSON.parse(response)["items"][0]["title"]}.to_json
+  end
+
+end
